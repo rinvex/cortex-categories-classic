037bee37d0d96e23d1d3202d5dd2eaaf4c8d46ef
Abdelrahman Omran <me@omranic.com>
2017-06-09 16:00:57 +0200

    Update datatable view location

--- src/Http/Controllers/Backend/CategoriesController.php
+++ src/Http/Controllers/Backend/CategoriesController.php
@@ -23,7 +23,7 @@ class CategoriesController extends AuthorizedController
      */
     public function index()
     {
-        return app(CategoriesDataTable::class)->render('cortex/foundation::backend.pages.datatable', ['resource' => 'cortex/categorizable::common.categories']);
+        return app(CategoriesDataTable::class)->render('cortex/foundation::backend.partials.datatable', ['resource' => 'cortex/categorizable::common.categories']);
     }
 
     /**
