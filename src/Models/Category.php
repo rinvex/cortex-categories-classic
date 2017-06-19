a02ff4c5c62ddd434351fe897be66c729caec6aa
Abdelrahman Omran <me@omranic.com>
2017-06-19 15:36:35 +0200

    Update model class docblock

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -15,24 +15,23 @@ use Rinvex\Categorizable\Category as BaseCategory;
  * @property array                                                                     $description
  * @property int                                                                       $_lft
  * @property int                                                                       $_rgt
- * @property int                                                                       $parent_id
- * @property \Carbon\Carbon                                                            $created_at
- * @property \Carbon\Carbon                                                            $updated_at
- * @property string                                                                    $deleted_at
+ * @property int|null                                                                  $parent_id
+ * @property \Carbon\Carbon|null                                                       $created_at
+ * @property \Carbon\Carbon|null                                                       $updated_at
+ * @property \Carbon\Carbon|null                                                       $deleted_at
  * @property-read \Kalnoy\Nestedset\Collection|\Cortex\Categorizable\Models\Category[] $children
- * @property-read \Cortex\Categorizable\Models\Category                                $parent
+ * @property-read \Cortex\Categorizable\Models\Category|null                           $parent
  *
- * @method static \Illuminate\Database\Query\Builder|\Rinvex\Categorizable\Category d()
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereDeletedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereDescription($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereLft($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereParentId($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereRgt($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereSlug($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Categorizable\Models\Category whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereLft($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereRgt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Category extends BaseCategory
