ff4bb491113a49355ef38324637250178141c319
Abdelrahman Omran <me@omranic.com>
2017-06-29 15:00:17 +0200

    Update model docblock

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -11,18 +11,19 @@ use Rinvex\Categorizable\Category as BaseCategory;
 /**
  * Cortex\Categorizable\Models\Category.
  *
- * @property int                                                                       $id
- * @property string                                                                    $slug
- * @property array                                                                     $name
- * @property array                                                                     $description
- * @property int                                                                       $_lft
- * @property int                                                                       $_rgt
- * @property int|null                                                                  $parent_id
- * @property \Carbon\Carbon|null                                                       $created_at
- * @property \Carbon\Carbon|null                                                       $updated_at
- * @property \Carbon\Carbon|null                                                       $deleted_at
- * @property-read \Kalnoy\Nestedset\Collection|\Cortex\Categorizable\Models\Category[] $children
- * @property-read \Cortex\Categorizable\Models\Category|null                           $parent
+ * @property int                                                                                 $id
+ * @property string                                                                              $slug
+ * @property array                                                                               $name
+ * @property array                                                                               $description
+ * @property int                                                                                 $_lft
+ * @property int                                                                                 $_rgt
+ * @property int|null                                                                            $parent_id
+ * @property \Carbon\Carbon|null                                                                 $created_at
+ * @property \Carbon\Carbon|null                                                                 $updated_at
+ * @property string|null                                                                         $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Activitylog\Models\Activity[] $activity
+ * @property-read \Kalnoy\Nestedset\Collection|\Cortex\Categorizable\Models\Category[]           $children
+ * @property-read \Cortex\Categorizable\Models\Category|null                                     $parent
  *
  * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Categorizable\Models\Category whereDeletedAt($value)
