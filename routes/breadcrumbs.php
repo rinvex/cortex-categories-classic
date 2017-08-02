6f5bbd8a35b2654f945a140062c3ebc1847d5ccb
Abdelrahman Omran <me@omranic.com>
2017-08-02 17:27:00 +0200

    Add breadcrumbs

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Cortex\Categorizable\Models\Category;
+use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator;
+
+Breadcrumbs::register('backend.categories.index', function (BreadcrumbsGenerator $breadcrumbs) {
+    $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.trans('cortex/foundation::common.backend'), route('backend.home'));
+    $breadcrumbs->push(trans('cortex/categorizable::common.categories'), route('backend.categories.index'));
+});
+
+Breadcrumbs::register('backend.categories.create', function (BreadcrumbsGenerator $breadcrumbs) {
+    $breadcrumbs->parent('backend.categories.index');
+    $breadcrumbs->push(trans('cortex/categorizable::common.create_category'), route('backend.categories.create'));
+});
+
+Breadcrumbs::register('backend.categories.edit', function (BreadcrumbsGenerator $breadcrumbs, Category $category) {
+    $breadcrumbs->parent('backend.categories.index');
+    $breadcrumbs->push($category->name, route('backend.categories.edit', ['category' => $category]));
+});
+
+Breadcrumbs::register('backend.categories.logs', function (BreadcrumbsGenerator $breadcrumbs, Category $category) {
+    $breadcrumbs->parent('backend.categories.index');
+    $breadcrumbs->push($category->name, route('backend.categories.edit', ['category' => $category]));
+    $breadcrumbs->push(trans('cortex/categorizable::common.logs'), route('backend.categories.logs', ['category' => $category]));
+});
