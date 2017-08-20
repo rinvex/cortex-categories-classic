1ccda86304e2af86b98ed139dfafb55de191a6ed
Abdelrahman Omran <me@omranic.com>
2017-08-20 03:07:44 +0200

    Change backend sidebar menu section registration

--- routes/menus.php
+++ routes/menus.php
@@ -2,4 +2,4 @@
 
 declare(strict_types=1);
 
-Menu::backendSidebar('resources')->routeIfCan('list-categories', 'backend.categories.index', '<i class="fa fa-sitemap"></i> <span>'.trans('cortex/categorizable::common.categories').'</span>');
+Menu::backendSidebar('taxonomies')->routeIfCan('list-categories', 'backend.categories.index', '<i class="fa fa-sitemap"></i> <span>'.trans('cortex/categorizable::common.categories').'</span>');
