cfa095561571a60ba4280523dec43f7f636ecbb2
Abdelrahman Omran <me@omranic.com>
2017-08-20 21:06:23 +0200

    Refactor SeedCommand to use the new SeederHelper

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace Cortex\Categorizable\Console\Commands;
 
 use Illuminate\Console\Command;
-use Rinvex\Fort\Traits\AbilitySeeder;
-use Rinvex\Fort\Traits\ArtisanHelper;
-use Illuminate\Support\Facades\Schema;
+use Rinvex\Support\Traits\SeederHelper;
 
 class SeedCommand extends Command
 {
-    use AbilitySeeder;
-    use ArtisanHelper;
+    use SeederHelper;
 
     /**
      * The name and signature of the console command.
@@ -37,37 +34,8 @@ class SeedCommand extends Command
     {
         $this->warn('Seed cortex/categorizable:');
 
-        if ($this->ensureExistingCategorizableTables()) {
-            // No seed data at the moment!
+        if ($this->ensureExistingDatabaseTables('rinvex/fort')) {
+            $this->seedResources(app('rinvex.fort.ability'), realpath(__DIR__.'/../../../resources/data/abilities.json'), ['name', 'description']);
         }
-
-        if ($this->ensureExistingFortTables()) {
-            $this->seedAbilities(realpath(__DIR__.'/../../../resources/data/abilities.json'));
-        }
-    }
-
-    /**
-     * Ensure existing categorizable tables.
-     *
-     * @return bool
-     */
-    protected function ensureExistingCategorizableTables()
-    {
-        if (! $this->hasCategorizableTables()) {
-            $this->call('cortex:migrate:categorizable');
-        }
-
-        return true;
-    }
-
-    /**
-     * Check if all required categorizable tables exists.
-     *
-     * @return bool
-     */
-    protected function hasCategorizableTables()
-    {
-        return Schema::hasTable(config('rinvex.categorizable.tables.categories'))
-               && Schema::hasTable(config('rinvex.categorizable.tables.categorizables'));
     }
 }
