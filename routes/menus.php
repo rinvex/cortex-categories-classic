01ed2e9946b98074f9dc51d40d46b59613d58117
Abdelrahman Omran <me@omranic.com>
2018-01-29 16:45:23 +0200

    Apply recent menu updates

--- routes/menus.php
+++ routes/menus.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 use Rinvex\Menus\Models\MenuItem;
-use Rinvex\Menus\Factories\MenuFactory;
+use Rinvex\Menus\Models\MenuGenerator;
 
-Menu::modify('adminarea.sidebar', function (MenuFactory $menu) {
+Menu::register('adminarea.sidebar', function (MenuGenerator $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.taxonomy'), 10, 'fa fa-arrows', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.categories.index'], trans('cortex/categories::common.categories'), 10, 'fa fa-sitemap')->ifCan('list-categories')->activateOnRoute('adminarea.categories');
     });
