8e34e0c8e8e0c26bf48c10c27d96c635c2fdfdd6
Abdelrahman Omran <me@omranic.com>
2018-03-24 02:59:20 +0200

    Tweak create form tabs

--- routes/menus.php
+++ routes/menus.php
@@ -13,7 +13,7 @@ Menu::register('adminarea.sidebar', function (MenuGenerator $menu, Category $cat
 });
 
 Menu::register('adminarea.categories.tabs', function (MenuGenerator $menu, Category $category) {
-    $menu->route(['adminarea.categories.create'], trans('cortex/categories::common.details'))->ifCan('create', $category)->if(! $category->exists);
+    $menu->route(['adminarea.categories.create'], trans('cortex/categories::common.details'))->ifCan('create', $category)->if(Route::is('adminarea.categories.create'));
     $menu->route(['adminarea.categories.edit', ['category' => $category]], trans('cortex/categories::common.details'))->ifCan('update', $category)->if($category->exists);
     $menu->route(['adminarea.categories.logs', ['category' => $category]], trans('cortex/categories::common.logs'))->ifCan('audit', $category)->if($category->exists);
 });
