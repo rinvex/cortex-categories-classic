80f6d99d0e653573bb31ac0064e6f63ea6c4072a
Abdelrahman Omran <me@omranic.com>
2018-03-24 01:09:50 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Adminarea/CategoriesController.php
+++ src/Http/Controllers/Adminarea/CategoriesController.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace Cortex\Categories\Http\Controllers\Adminarea;
 
 use Cortex\Categories\Models\Category;
-use Cortex\Foundation\DataTables\ImportLogsDataTable;
-use Cortex\Foundation\Http\Requests\ImportFormRequest;
-use Cortex\Foundation\Importers\DefaultImporter;
 use Illuminate\Foundation\Http\FormRequest;
 use Cortex\Foundation\DataTables\LogsDataTable;
+use Cortex\Foundation\Importers\DefaultImporter;
+use Cortex\Foundation\DataTables\ImportLogsDataTable;
+use Cortex\Foundation\Http\Requests\ImportFormRequest;
 use Cortex\Foundation\Http\Controllers\AuthorizedController;
 use Cortex\Categories\DataTables\Adminarea\CategoriesDataTable;
 use Cortex\Categories\Http\Requests\Adminarea\CategoryFormRequest;
