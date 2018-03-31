954fb1b2f1080859133dbeb2e81588785dc6dc79
Abdelrahman Omran <me@omranic.com>
2018-03-31 22:35:11 +0200

    Escape datatable rows

--- src/Transformers/Adminarea/CategoryTransformer.php
+++ src/Transformers/Adminarea/CategoryTransformer.php
@@ -4,21 +4,24 @@ declare(strict_types=1);
 
 namespace Cortex\Categories\Transformers\Adminarea;
 
+use Rinvex\Support\Traits\Escaper;
 use Cortex\Categories\Models\Category;
 use League\Fractal\TransformerAbstract;
 
 class CategoryTransformer extends TransformerAbstract
 {
+    use Escaper;
+
     /**
      * @return array
      */
     public function transform(Category $category): array
     {
-        return [
+        return $this->escapeRow([
             'id' => (string) $category->getRouteKey(),
             'name' => (string) $category->name,
             'created_at' => (string) $category->created_at,
             'updated_at' => (string) $category->updated_at,
-        ];
+        ]);
     }
 }
