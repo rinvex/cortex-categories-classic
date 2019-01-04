30001f1bce75cd8ffecd34c6430018044f540c25
Abdelrahman Omran <me@omranic.com>
2019-01-04 05:34:36 +0200

    Utilize includeWhen blade directive

--- resources/views/adminarea/pages/category.blade.php
+++ resources/views/adminarea/pages/category.blade.php
@@ -13,9 +13,7 @@
 {{-- Main Content --}}
 @section('content')
 
-    @if($category->exists)
-        @include('cortex/foundation::common.partials.modal', ['id' => 'delete-confirmation'])
-    @endif
+    @includeWhen($category->exists, 'cortex/foundation::common.partials.modal', ['id' => 'delete-confirmation'])
 
     <div class="content-wrapper">
         <section class="content-header">
