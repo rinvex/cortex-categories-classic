c3d2bee7969506f2e66be75386cbe6ef3a4b77e3
Abdelrahman Omran <me@omranic.com>
2019-10-14 01:07:02 +0000

    Update menus & breadcrumbs event listener to accessarea.ready

--- src/Providers/CategoriesServiceProvider.php
+++ src/Providers/CategoriesServiceProvider.php
@@ -73,7 +73,7 @@ class CategoriesServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'cortex/categories');
         $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'cortex/categories');
 
-        $this->app->runningInConsole() || $dispatcher->listen('controller.constructed', function ($accessarea) {
+        $this->app->runningInConsole() || $dispatcher->listen('accessarea.ready', function ($accessarea) {
             ! file_exists($menus = __DIR__."/../../routes/menus/{$accessarea}.php") || require $menus;
             ! file_exists($breadcrumbs = __DIR__."/../../routes/breadcrumbs/{$accessarea}.php") || require $breadcrumbs;
         });
