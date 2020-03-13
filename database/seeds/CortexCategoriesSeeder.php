dc86da8bd9e50e67727f2d63786522dc0c5a001e
Abdelrahman Omran <me@omranic.com>
2020-03-13 18:01:39 +0400

    Check if ability exists before seeding

--- database/seeds/CortexCategoriesSeeder.php
+++ database/seeds/CortexCategoriesSeeder.php
@@ -23,7 +23,10 @@ class CortexCategoriesSeeder extends Seeder
         ];
 
         collect($abilities)->each(function (array $ability) {
-            app('cortex.auth.ability')->create($ability);
+            app('cortex.auth.ability')->firstOrCreate([
+                'name' => $ability['name'],
+                'entity_type' => $ability['entity_type'],
+            ], $ability);
         });
     }
 }
