8d7b28794ffaf52e3aebffb35293785216dea0f7
Abdelrahman Omran <me@omranic.com>
2020-03-30 05:55:18 +0400

    Enforce consistent artisan command tag namespacing

--- src/Console/Commands/PublishCommand.php
+++ src/Console/Commands/PublishCommand.php
@@ -33,18 +33,18 @@ class PublishCommand extends BasePublishCommand
 
         switch ($this->option('resource')) {
             case 'lang':
-                $this->call('vendor:publish', ['--tag' => 'cortex-categories-lang', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/categories::lang', '--force' => $this->option('force')]);
                 break;
             case 'views':
-                $this->call('vendor:publish', ['--tag' => 'cortex-categories-views', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/categories::views', '--force' => $this->option('force')]);
                 break;
             case 'migrations':
-                $this->call('vendor:publish', ['--tag' => 'cortex-categories-migrations', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/categories::migrations', '--force' => $this->option('force')]);
                 break;
             default:
-                $this->call('vendor:publish', ['--tag' => 'cortex-categories-lang', '--force' => $this->option('force')]);
-                $this->call('vendor:publish', ['--tag' => 'cortex-categories-views', '--force' => $this->option('force')]);
-                $this->call('vendor:publish', ['--tag' => 'cortex-categories-migrations', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/categories::lang', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/categories::views', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/categories::migrations', '--force' => $this->option('force')]);
                 break;
         }
 
