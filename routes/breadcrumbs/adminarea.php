4e6d2aedddb1ef4042f4028d1a1cff5c4b67db1e
Abdelrahman Omran <me@omranic.com>
2021-05-25 18:23:51 +0400

    Replace deprecated `Breadcrumbs::register` with `Breadcrumbs::for`

--- routes/breadcrumbs/adminarea.php
+++ routes/breadcrumbs/adminarea.php
@@ -6,33 +6,33 @@ use Diglactic\Breadcrumbs\Generator;
 use Cortex\Categories\Models\Category;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
-Breadcrumbs::register('adminarea.cortex.categories.categories.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.categories.categories.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('adminarea.home'));
     $breadcrumbs->push(trans('cortex/categories::common.categories'), route('adminarea.cortex.categories.categories.index'));
 });
 
-Breadcrumbs::register('adminarea.cortex.categories.categories.import', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.categories.categories.import', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.categories.categories.index');
     $breadcrumbs->push(trans('cortex/categories::common.import'), route('adminarea.cortex.categories.categories.import'));
 });
 
-Breadcrumbs::register('adminarea.cortex.categories.categories.import.logs', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.categories.categories.import.logs', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.categories.categories.index');
     $breadcrumbs->push(trans('cortex/categories::common.import'), route('adminarea.cortex.categories.categories.import'));
     $breadcrumbs->push(trans('cortex/categories::common.logs'), route('adminarea.cortex.categories.categories.import.logs'));
 });
 
-Breadcrumbs::register('adminarea.cortex.categories.categories.create', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.categories.categories.create', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.categories.categories.index');
     $breadcrumbs->push(trans('cortex/categories::common.create_category'), route('adminarea.cortex.categories.categories.create'));
 });
 
-Breadcrumbs::register('adminarea.cortex.categories.categories.edit', function (Generator $breadcrumbs, Category $category) {
+Breadcrumbs::for('adminarea.cortex.categories.categories.edit', function (Generator $breadcrumbs, Category $category) {
     $breadcrumbs->parent('adminarea.cortex.categories.categories.index');
     $breadcrumbs->push(strip_tags($category->name), route('adminarea.cortex.categories.categories.edit', ['category' => $category]));
 });
 
-Breadcrumbs::register('adminarea.cortex.categories.categories.logs', function (Generator $breadcrumbs, Category $category) {
+Breadcrumbs::for('adminarea.cortex.categories.categories.logs', function (Generator $breadcrumbs, Category $category) {
     $breadcrumbs->parent('adminarea.cortex.categories.categories.index');
     $breadcrumbs->push(strip_tags($category->name), route('adminarea.cortex.categories.categories.edit', ['category' => $category]));
     $breadcrumbs->push(trans('cortex/categories::common.logs'), route('adminarea.cortex.categories.categories.logs', ['category' => $category]));
