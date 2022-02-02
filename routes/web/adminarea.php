f392b9b5160efc5a0acabf9e48d45c36cfd09f61
Abdelrahman Omran <me@omranic.com>
2022-02-02 10:37:34 +0400

    Update routes to use class based definitions

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -2,26 +2,32 @@
 
 declare(strict_types=1);
 
+use Cortex\Categories\Http\Controllers\Adminarea\CategoriesController;
+
 Route::domain('{adminarea}')->group(function () {
     Route::name('adminarea.')
-         ->namespace('Cortex\Categories\Http\Controllers\Adminarea')
          ->middleware(['web', 'nohttpcache', 'can:access-adminarea'])
          ->prefix(route_prefix('adminarea'))->group(function () {
 
         // Categories Routes
              Route::name('cortex.categories.categories.')->prefix('categories')->group(function () {
-                 Route::match(['get', 'post'], '/')->name('index')->uses('CategoriesController@index');
-                 Route::get('import')->name('import')->uses('CategoriesController@import');
-                 Route::post('import')->name('stash')->uses('CategoriesController@stash');
-                 Route::post('hoard')->name('hoard')->uses('CategoriesController@hoard');
-                 Route::get('import/logs')->name('import.logs')->uses('CategoriesController@importLogs');
-                 Route::get('create')->name('create')->uses('CategoriesController@create');
-                 Route::post('create')->name('store')->uses('CategoriesController@store');
-                 Route::get('{category}')->name('show')->uses('CategoriesController@show');
-                 Route::get('{category}/edit')->name('edit')->uses('CategoriesController@edit');
-                 Route::put('{category}/edit')->name('update')->uses('CategoriesController@update');
-                 Route::match(['get', 'post'], '{category}/logs')->name('logs')->uses('CategoriesController@logs');
-                 Route::delete('{category}')->name('destroy')->uses('CategoriesController@destroy');
+                 Route::match(['get', 'post'], '/')->name('index')->uses([CategoriesController::class, 'index']);
+                 Route::get('import')->name('import')->uses([CategoriesController::class, 'import']);
+                 Route::post('import')->name('stash')->uses([CategoriesController::class, 'stash']);
+                 Route::post('hoard')->name('hoard')->uses([CategoriesController::class, 'hoard']);
+                 Route::get('import/logs')->name('import.logs')->uses([CategoriesController::class, 'importLogs']);
+                 Route::get('create')->name('create')->uses([CategoriesController::class, 'create']);
+                 Route::post('create')->name('store')->uses([CategoriesController::class, 'store']);
+                 Route::get('{category}')->name('show')->uses([CategoriesController::class, 'show']);
+                 Route::get('{category}/edit')->name('edit')->uses([CategoriesController::class, 'edit']);
+                 Route::put('{category}/edit')->name('update')->uses([CategoriesController::class, 'update']);
+                 Route::match(['get', 'post'], '{category}/logs')->name('logs')->uses([CategoriesController::class, 'logs']);
+                 Route::delete('{category}')->name('destroy')->uses([CategoriesController::class, 'destroy']);
+
+                 Route::name('ajax.')->prefix('ajax')->group(function () {
+                     Route::get('{category}/children')->name('get.children')->uses([CategoriesController::class, 'nodeChildren']);
+                     Route::post('{category}/update-position')->name('update.position')->uses([CategoriesController::class, 'updateNodePosition']);
+                 });
              });
          });
 });
